cbc0fc613a801e2711446dcda6c62be1ddef77c8
Murtada Altaee <murtada.altaee@gmail.com>
2020-06-11 15:23:07 +0300

    init

---
+++ lib/route_generator.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'src/models/route_argument.dart';
+import 'src/pages/cart.dart';
+import 'src/pages/category.dart';
+import 'src/pages/debug.dart';
+import 'src/pages/details.dart';
+import 'src/pages/languages.dart';
+import 'src/pages/login.dart';
+import 'src/pages/menu_list.dart';
+import 'src/pages/order_success.dart';
+import 'src/pages/pages.dart';
+import 'src/pages/product.dart';
+import 'src/pages/profile.dart';
+import 'src/pages/settings.dart';
+import 'src/pages/signup.dart';
+import 'src/pages/splash_screen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    // Getting arguments passed in while calling Navigator.pushNamed
+    final args = settings.arguments;
+    switch (settings.name) {
+      case '/Debug':
+        return MaterialPageRoute(builder: (_) => DebugWidget(routeArgument: args as RouteArgument));
+      case '/Splash':
+        return MaterialPageRoute(builder: (_) => SplashScreen());
+      case '/SignUp':
+        return MaterialPageRoute(builder: (_) => SignUpWidget());
+      case '/MobileVerification':
+        return MaterialPageRoute(builder: (_) => SignUpWidget());
+      case '/MobileVerification2':
+        return MaterialPageRoute(builder: (_) => SignUpWidget());
+      case '/Login':
+        return MaterialPageRoute(builder: (_) => LoginWidget());
+      case '/Profile':
+        return MaterialPageRoute(builder: (_) => ProfileWidget());
+      case '/Pages':
+        return MaterialPageRoute(builder: (_) => PagesWidget(currentTab: args));
+      case '/Details':
+        return MaterialPageRoute(builder: (_) => DetailsWidget(routeArgument: args as RouteArgument));
+      case '/Menu':
+        return MaterialPageRoute(builder: (_) => MenuWidget(routeArgument: args as RouteArgument));
+      case '/Product':
+        return MaterialPageRoute(builder: (_) => ProductWidget(routeArgument: args as RouteArgument));
+      case '/Category':
+        return MaterialPageRoute(builder: (_) => CategoryWidget(routeArgument: args as RouteArgument));
+      case '/Cart':
+        return MaterialPageRoute(builder: (_) => CartWidget(routeArgument: args as RouteArgument));
+      case '/CashOnDelivery':
+        return MaterialPageRoute(builder: (_) => OrderSuccessWidget(routeArgument: RouteArgument(param: 'Cash on Delivery')));
+      case '/PayOnPickup':
+        return MaterialPageRoute(builder: (_) => OrderSuccessWidget(routeArgument: RouteArgument(param: 'Pay on Pickup')));
+      case '/OrderSuccess':
+        return MaterialPageRoute(builder: (_) => OrderSuccessWidget(routeArgument: args as RouteArgument));
+      case '/Languages':
+        return MaterialPageRoute(builder: (_) => LanguagesWidget());
+      case '/Settings':
+        return MaterialPageRoute(builder: (_) => SettingsWidget());
+      default:
+        // If there is no such named route in the switch statement, e.g. /third
+        return MaterialPageRoute(builder: (_) => Scaffold(body: SafeArea(child: Text('Route Error'))));
+    }
+  }
+}
